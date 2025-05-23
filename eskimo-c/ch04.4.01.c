b2ddc5c07739e4aa0e780844ceeff5cddae5acca
Dave For Projects <daveforprojects@gmail.com>
2025-05-23 00:18:50 -0400

    static local persistence

--- eskimo-c/ch04.4.01.c
+++ eskimo-c/ch04.4.01.c
@@ -17,8 +17,18 @@ gcc -ansi -m32 ch04.4.01.c ch04.4.02.c -o prog  && $(pwd)/prog
 void func_01(){
    printf("func_01\n");
    printf("%s\n", var_04);
-
 }
+
+
+void func_03(){
+   static int var_05; 
+   
+   printf("func_03: ");
+   printf("%d ", var_05);
+   var_05++;
+   printf("%d ", var_05);
+}
+
 
 int main(){
 
@@ -28,12 +38,20 @@ int main(){
    printf("%s\n", var_01);  /* Local scope overrides global */
    printf("%s\n", var_02);  /* Global scope */
 
-   if(var_03)                      /* Var defined in other file is available */
+   if(var_03)                   /* Var defined in other file is available */
       printf("%s\n", var_03);
 
    if(var_04){
       func_01();
-      func_02();                  /* Static variable not available externally */
+      func_02();                /* Static variable not available externally */
    }
+
+   printf("func_03 1\n");       /* Static local variable should persist */
+   func_03();
+   printf("\n");
+   printf("func_03 2\n");
+   func_03();
+   printf("\n");
+
    return 0;
 }
