5df8575a0744ea3059f14f226c45ec2de8734bba
Dave For Projects <daveforprojects@gmail.com>
2025-06-11 11:45:48 -0400

    If-else logical or

---
+++ cs50/agree.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+    char c = get_char("Do you agree: ");
+
+    if (( c == 'y' ) || (c == 'Y'))
+    {
+        printf("Yes\n");
+    }
+    else if (( c == 'n' ) || ( c == 'N' ))
+    {
+        printf("No\n");
+    }
+    else
+    {
+        printf("Bad response\n");
+    }
+
+    return 0;
+}
