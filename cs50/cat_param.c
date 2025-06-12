d189caa98f059ffa3efdc4a8abe061fabaa0cff1
Dave For Projects <daveforprojects@gmail.com>
2025-06-11 23:43:32 -0400

    Parameterize function

---
+++ cs50/cat_param.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+#define NUM_REPETITIONS 3
+
+void meow(int);   /* Function prototypes */
+
+int main()         /* main() first for style */
+{
+    meow(NUM_REPETITIONS);
+
+    return 0;
+}
+
+void meow(int repetitions)     /* Function definitions */
+{
+    int i;
+
+    for (i=0; i < repetitions ; i++){
+        printf("meow\n");
+    }
+
+}
