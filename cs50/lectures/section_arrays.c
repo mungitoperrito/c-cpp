d7cba6f4384c2d154dc503d2be3e87ff8ded12e9
Dave For Projects <daveforprojects@gmail.com>
2025-06-17 12:49:48 -0400

    Exercise arrays 01

--- cs50/lectures/section_arrays.c
+++ cs50/lectures/section_arrays.c
@@ -8,9 +8,24 @@ int main(void)
     int i;
     int multiples[NUM_VALUES];
 
+    // Double new assignment
     for (i = 0; i < NUM_VALUES; i++)
     {
         multiples[i] = 2 * (i + 1);
+    }
+
+    for (i = 0; i < NUM_VALUES; i++)
+    {
+        printf("%d ", multiples[i]);
+    }
+    printf("\n");
+
+    // Double prior value
+    multiples[0] = 1;  //Base case
+
+    for (i = 1; i < NUM_VALUES; i++)
+    {
+        multiples[i] = 2 * multiples[i-1];
     }
 
     for (i = 0; i < NUM_VALUES; i++)
